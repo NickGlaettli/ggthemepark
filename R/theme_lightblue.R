a9e7f0ed396424cec588e60093c059783d184840
Nick Glaettli <nick.glaettli@gmail.com>
2022-01-25 22:14:01 +0100

    Adjust caption

--- R/theme_lightblue.R
+++ R/theme_lightblue.R
@@ -27,6 +27,7 @@ theme_lightblue <- function(yline = F) {
 
       plot.title= element_text(face="bold",family="serif", size = 15, hjust = 0.5, vjust = 4),
       plot.subtitle = element_text(family="serif", size= 13, hjust = 0.5, vjust = 3),
+      plot.caption = element_text(hjust = 0),
 
       #Axis lines
       axis.line.x = element_line(),
--- R/theme_simple.R
+++ R/theme_simple.R
@@ -19,12 +19,13 @@ theme_simple <- function(yline = F) {
 
       #Grid
       panel.grid.minor = element_blank(),
-      panel.grid.major.x = element_blank(),,
+      panel.grid.major.x = element_blank(),
 
       #Labs
       legend.position = "bottom",
       plot.title= element_text(face="bold",family="Helvetica", size = 15, hjust = 0.5, vjust = 4),
       plot.subtitle = element_text(family="Helvetica", size= 13, hjust = 0.5, vjust = 3),
+      plot.caption = element_text(hjust = 0),
 
       #Extra line
       axis.line.x = element_line(),
